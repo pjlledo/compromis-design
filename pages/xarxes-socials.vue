0ea0c68dc0054a189398d8cf51fe755fdc00d093
Carles <carles2612@gmail.com>
2019-11-29 14:19:53 +0100

    Add social networks section

--- pages/xarxes-socials.vue
+++ pages/xarxes-socials.vue
@@ -1,6 +1,39 @@
 <template>
   <div>
     <h1>Xarxes socials</h1>
+    <div class="row">
+      <div class="col-md-8">
+        <h2>Foto de perfil</h2>
+        <p>La foto de perfil que emprem a les xarxes socials està composada d'una foto representativa del municipi o institució i la careta de Compromís.</p>
+        <p>Empreu l'eina oficial per generar la foto de perfil del vostre col·lectiu.</p>
+        <figure class="text-center">
+          <img src="../assets/xarxes/icona.png" alt="Icona per a xarxes" style="max-width: 200px; border-radius: 50%;">
+        </figure>
+      </div>
+      <div class="col-md-4 d-flex align-items-end">
+        <p>
+          <c-button href="https://targes.compromis.net/#/social" variant="outline-primary" size="lg" target="_blank">
+            Crea una foto de perfil
+          </c-button>
+        </p>
+      </div>
+    </div>
+
+    <h2>Capçalera</h2>
+    <div class="row">
+      <div class="col-md-8">
+        <p>Com a capçalera, és recomanable emprar una foto representativa del municipi, com una panoràmica o una foto d'un monument representatiu.</p>
+
+        <p>Eviteu afegir més elements, com el logo de compromís o altres xarxes socials, sobre la foto si no és estrictament necessari</p>
+
+        <figure class="edge">
+          <img src="../assets/xarxes/header.jpg" alt="">
+        </figure>
+      </div>
+    </div>
+
+    <h2>Targes</h2>
+    <p>..</p>
   </div>
 </template>
 
