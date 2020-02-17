99ddc85c9a10830ea0d3d62aa995df15f78c2f38
Guillem <guillem@disedit.com>
2020-02-17 13:32:35 +0100

    Add backdrop filter for text outside of cards

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="{'container': !inPage, 'in-page': inPage}">
-    <footer>
+    <footer class="cardless-text">
       <p>Si tens qualsevol dubte o suggeriment, pots contactar-nos en <a href="mailto:disseny@compromis.net">disseny@compromis.net</a></p>
       <p>
         <strong>Manual d'Estil de Compromís</strong>.
@@ -43,22 +43,11 @@ export default {
 }
 
 footer {
-  background: rgba($body-bg, .75);
-  backdrop-filter: blur(4px);
-  padding: 2rem;
-  border-radius: $border-radius;
-  color: $gray-600;
   margin-bottom: 2rem;
   margin-top: 10rem;
-  margin-left: -2rem;
-  margin-right: -2rem;
 
   p {
     margin-bottom: .5rem;
-  }
-
-  p:last-child {
-    margin-bottom: 0;
   }
 }
 
@@ -86,13 +75,6 @@ footer {
       background: $gray-400;
       top: .5rem;
     }
-  }
-}
-
-@include media-breakpoint-down(lg) {
-  footer {
-    margin-left: 0;
-    margin-right: 0;
   }
 }
 </style>
