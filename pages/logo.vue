efd9b8a2a342354b74384888edb4fba539a740ee
Pedro J. Lledó Sigüenza <mantha@iMac-de-Pedro.local>
2020-05-06 18:22:09 +0200

    corrections

--- pages/logo.vue
+++ pages/logo.vue
@@ -125,7 +125,7 @@
                 >
               </figure>
               <figcaption>Logo original</figcaption>
-              <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
+              <div class="col-lg-4 col-md-6">
                 <figure class="edge no-margin text-center">
                   <img
                     src="../assets/logo/Joves PV + Compromis Sin miniatura Gris.png"
@@ -137,8 +137,6 @@
                 </figcaption>
               </div>
             </div>
-          </div>
-          <div class="row">
             <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
               <figure class="edge no-margin text-center">
                 <img
